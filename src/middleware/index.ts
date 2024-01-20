ddfdda7e55e867590fe8280cc7d7528299fb0741
djangcy <4connerspence@gmail.com>
2024-01-20 15:40:09 +0200

    remove: dead code

--- src/middleware/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './allowed_methods';
-export * from './key_verification';
-export * from './timeout';
--- src/utils/get_field_value_pairs.ts
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash';
-
-export function removeUndefinedValues(object: any): {} {
-  // Use lodash pickBy to exclude undefined values
-  return _.pickBy(object, (value) => value !== undefined);
-}
